cdefee92bc333f26443867047dc619ae739b2e5e
K900 <k0009000@gmail.com>
2011-06-12 22:50:45 +0400

    New command syntax, fixes, hide: config nodes.

---
+++ src/com/ubempire/render/CopierThread.java
@@ -0,0 +1,11 @@
+package com.ubempire.render;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: K900
+ * Date: 12.06.11
+ * Time: 22:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class CopierThread {
+}
