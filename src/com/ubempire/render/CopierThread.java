f816cb99a22bee8b6e8eef254073557ac1bf5426
K900 <k0009000@gmail.com>
2011-06-12 23:01:07 +0400

    Some more last minute fixes :)

--- src/com/ubempire/render/CopierThread.java
+++ src/com/ubempire/render/CopierThread.java
@@ -1,5 +1,7 @@
 package com.ubempire.render;
 
+import java.io.*;
+
 /**
  * Created by IntelliJ IDEA.
  * User: K900
@@ -7,5 +9,54 @@ package com.ubempire.render;
  * Time: 22:27
  * To change this template use File | Settings | File Templates.
  */
-public class CopierThread {
+public class CopierThread extends Thread {
+    private File copyTo;
+    private File copyFrom;
+
+    // A snippet I Googled. Laaa-zyy... :P
+    public void copy(File sourceLocation , File targetLocation) throws IOException {
+        if (!sourceLocation.exists())
+        {
+             System.out.println("Source files not found! Please check if you have everything set up correctly.");
+        }
+        if (sourceLocation.isDirectory()) {
+            if (!targetLocation.exists()) {
+                targetLocation.mkdir();
+            }
+
+            String[] children = sourceLocation.list();
+            for (int i=0; i<children.length; i++) {
+                copy(new File(sourceLocation, children[i]),
+                        new File(targetLocation, children[i]));
+            }
+        } else {
+
+            InputStream in = new FileInputStream(sourceLocation);
+            OutputStream out = new FileOutputStream(targetLocation);
+
+            // Copy the bits from instream to outstream
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        }
+    }
+
+    CopierThread(File copyFrom, File copyTo) {
+        super();
+        this.copyFrom = copyFrom;
+        this.copyTo = copyTo;
+    }
+
+    public void run() {
+        try {
+            System.out.println("Done copying files.");
+            copy(copyFrom, copyTo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
